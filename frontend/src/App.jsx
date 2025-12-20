22b6edd67f9a533b95e1bc50f5edefa208a236fc
loneandhungry <mstejaswani@gmail.com>
2025-12-20 23:51:29 +0530

    Some more cahnges to frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ import Template2 from './Component/template2';
 import axios from "axios";
 import Signup from './Component/signup';
 import Landing from './Component/landing';
-import dotenv from "dotenv"
+
 
 
 function App(){
